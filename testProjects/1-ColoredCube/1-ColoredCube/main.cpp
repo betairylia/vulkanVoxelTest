22c899fcb453dcdc790085b0aafe7d1af5c35083
linkzeldagg <visual_11@126.com>
2016-05-31 23:25:17 +0800

    swapChain finished. but it doesn't work, vkCreateWin32SurfaceKHR returns VK_SUCCESS but no surface data has returned. maybe i need to do it in a win32 application?(WINMAIN)..? who knows lol.

--- testProjects/1-ColoredCube/1-ColoredCube/main.cpp
+++ testProjects/1-ColoredCube/1-ColoredCube/main.cpp
@@ -1,3 +1,7 @@
+#define WIN32_LEAN_AND_MEAN
+#define VK_USE_PLATFORM_WIN32_KHR
+#define NOMINMAX /* Don't let Windows define min() or max() */
+
 #include <iostream>
 #include <cstdlib>
 #include <string>
@@ -11,6 +15,16 @@
 using namespace std;
 
 /*
+	Structs
+*/
+
+typedef struct _swap_chain_buffers {
+	VkImage image;
+	VkImageView view;
+} swap_chain_buffer;
+
+
+/*
 	Globals
 */
 
@@ -18,13 +32,31 @@ VkInstance m_instance;
 vector<VkPhysicalDevice> m_GPUs;
 
 uint32_t m_queueCount;
+uint32_t m_queueFamilyIndex;//index of the queue founded
 vector<VkQueueFamilyProperties> m_queueProps;
 
 VkDevice m_device;
 
+VkCommandPool m_cmdPool;
+VkCommandBuffer m_cmdBuffer;
+
+VkSurfaceKHR m_surface;
+uint32_t m_queueGraphicsPresentIndex;
+VkQueue m_queue;
+
+VkSwapchainKHR m_swapChain;
+VkFormat m_colorImgFormat;
+uint32_t m_swapChainImageCount;
+vector<swap_chain_buffer> m_swapChainImgBuffer;
+
 ///win32
 HINSTANCE m_connection;
 HWND m_window;
+
+///const
+const int fenceTimeout = 100000000;
+const int width = 800;
+const int height = 600;
 
 /*
 	Win32 window
@@ -50,7 +82,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	return (DefWindowProc(hWnd, uMsg, wParam, lParam));
 }
 
-void initWindow(int width, int height, string windowTitle)
+void InitWindow(int width, int height, string windowTitle)
 {
 	//char title[100];
 	//sprintf(title, "%s", windowTitle.c_str());
@@ -103,6 +135,123 @@ void initWindow(int width, int height, string windowTitle)
 		exit(1);
 	}
 	//SetWindowLongPtr(m_window, GWLP_USERDATA, (LONG_PTR)&info);
+}
+
+
+/*
+	ToolBoxes
+*/
+
+void BeginCommandBuffer(VkCommandBuffer &cmdBuf)
+{
+	VkCommandBufferBeginInfo cmd_buf_info = {};
+	cmd_buf_info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+	cmd_buf_info.pNext = NULL;
+	cmd_buf_info.flags = 0;
+	cmd_buf_info.pInheritanceInfo = NULL;
+
+	VkResult res = vkBeginCommandBuffer(cmdBuf, &cmd_buf_info);
+	assert(res == VK_SUCCESS);
+}
+
+void EndCommandBuffer(VkCommandBuffer &cmdBuf)
+{
+	VkResult res = vkEndCommandBuffer(cmdBuf);
+	assert(res == VK_SUCCESS);
+}
+
+void QueueCommandBuffer(VkCommandBuffer &cmdBuf, VkDevice &device, VkQueue &queue)
+{
+	VkResult res;
+
+	/* Queue the command buffer for execution */
+	const VkCommandBuffer cmd_bufs[] = { cmdBuf };
+	VkFenceCreateInfo fenceInfo;
+	VkFence drawFence;
+	fenceInfo.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
+	fenceInfo.pNext = NULL;
+	fenceInfo.flags = 0;
+	vkCreateFence(device, &fenceInfo, NULL, &drawFence);
+
+	VkPipelineStageFlags pipe_stage_flags =
+		VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT;
+	VkSubmitInfo submit_info[1] = {};
+	submit_info[0].pNext = NULL;
+	submit_info[0].sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+	submit_info[0].waitSemaphoreCount = 0;
+	submit_info[0].pWaitSemaphores = NULL;
+	submit_info[0].pWaitDstStageMask = &pipe_stage_flags;
+	submit_info[0].commandBufferCount = 1;
+	submit_info[0].pCommandBuffers = cmd_bufs;
+	submit_info[0].signalSemaphoreCount = 0;
+	submit_info[0].pSignalSemaphores = NULL;
+
+	res = vkQueueSubmit(queue, 1, submit_info, drawFence);
+	assert(res == VK_SUCCESS);
+
+	do {
+		res =
+			vkWaitForFences(device, 1, &drawFence, VK_TRUE, fenceTimeout);
+	} while (res == VK_TIMEOUT);
+	assert(res == VK_SUCCESS);
+
+	vkDestroyFence(device, drawFence, NULL);
+}
+
+void set_image_layout(VkCommandBuffer &cmdBuf, VkQueue &queue, VkImage image,
+	VkImageAspectFlags aspectMask,
+	VkImageLayout old_image_layout,
+	VkImageLayout new_image_layout) {
+	/* DEPENDS on info.cmd and info.queue initialized */
+
+	assert(cmdBuf != VK_NULL_HANDLE);
+	assert(queue != VK_NULL_HANDLE);
+
+	VkImageMemoryBarrier image_memory_barrier = {};
+	image_memory_barrier.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
+	image_memory_barrier.pNext = NULL;
+	image_memory_barrier.srcAccessMask = 0;
+	image_memory_barrier.dstAccessMask = 0;
+	image_memory_barrier.oldLayout = old_image_layout;
+	image_memory_barrier.newLayout = new_image_layout;
+	image_memory_barrier.image = image;
+	image_memory_barrier.subresourceRange.aspectMask = aspectMask;
+	image_memory_barrier.subresourceRange.baseMipLevel = 0;
+	image_memory_barrier.subresourceRange.levelCount = 1;
+	image_memory_barrier.subresourceRange.layerCount = 1;
+
+	if (old_image_layout == VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL) {
+		image_memory_barrier.srcAccessMask =
+			VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
+	}
+
+	if (new_image_layout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL) {
+		/* Make sure anything that was copying from this image has completed */
+		image_memory_barrier.dstAccessMask = VK_ACCESS_MEMORY_READ_BIT;
+	}
+
+	if (new_image_layout == VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL) {
+		/* Make sure any Copy or CPU writes to image are flushed */
+		image_memory_barrier.srcAccessMask =
+			VK_ACCESS_HOST_WRITE_BIT | VK_ACCESS_TRANSFER_WRITE_BIT;
+		image_memory_barrier.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
+	}
+
+	if (new_image_layout == VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL) {
+		image_memory_barrier.dstAccessMask =
+			VK_ACCESS_COLOR_ATTACHMENT_READ_BIT;
+	}
+
+	if (new_image_layout == VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL) {
+		image_memory_barrier.dstAccessMask =
+			VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_READ_BIT;
+	}
+
+	VkPipelineStageFlags src_stages = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
+	VkPipelineStageFlags dest_stages = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
+
+	vkCmdPipelineBarrier(cmdBuf, src_stages, dest_stages, 0, 0, NULL, 0, NULL,
+		1, &image_memory_barrier);
 }
 
 
@@ -160,7 +309,9 @@ void EnumeratePhysicalDevices(VkInstance &inst, vector<VkPhysicalDevice> &gpus)
 	assert(!res && gpu_count >= 1);
 }
 
-VkDevice CreateDevice(vector<VkPhysicalDevice> &gpus, uint32_t &queueCount, vector<VkQueueFamilyProperties> &queueProps)
+VkDevice CreateDevice(
+	vector<VkPhysicalDevice> &gpus, uint32_t &queueCount, 
+	vector<VkQueueFamilyProperties> &queueProps, uint32_t &familyIndex)
 {
 	VkDeviceQueueCreateInfo queue_info = {};
 
@@ -175,7 +326,7 @@ VkDevice CreateDevice(vector<VkPhysicalDevice> &gpus, uint32_t &queueCount, vect
 	bool found = false;
 	for (unsigned int i = 0; i < queueCount; i++) {
 		if (queueProps[i].queueFlags & VK_QUEUE_GRAPHICS_BIT) {
-			queue_info.queueFamilyIndex = i;
+			familyIndex = queue_info.queueFamilyIndex = i;
 			found = true;
 			break;
 		}
@@ -208,6 +359,272 @@ VkDevice CreateDevice(vector<VkPhysicalDevice> &gpus, uint32_t &queueCount, vect
 	return device;
 }
 
+VkCommandPool CreateCommandPool(const uint32_t queueFamilyIndex, VkDevice &device)
+{
+	VkCommandPool cmdPool;
+
+	VkCommandPoolCreateInfo cmd_pool_info = {};
+	cmd_pool_info.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+	cmd_pool_info.pNext = NULL;
+	cmd_pool_info.queueFamilyIndex = queueFamilyIndex;
+	
+	//TODO: Maybe we need modify flags here.
+	cmd_pool_info.flags = 0; 
+
+	VkResult res =
+		vkCreateCommandPool(device, &cmd_pool_info, NULL, &cmdPool);
+	assert(res == VK_SUCCESS);
+
+	return cmdPool;
+}
+
+VkCommandBuffer CreateCommandBuffer(VkCommandPool &cmdPool, VkDevice &device)
+{
+	VkCommandBuffer cmdBuf;
+
+	VkCommandBufferAllocateInfo cmd = {};
+	cmd.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+	cmd.pNext = NULL;
+	cmd.commandPool = cmdPool;
+	cmd.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+	cmd.commandBufferCount = 1;
+
+	VkResult res = vkAllocateCommandBuffers(device, &cmd, &cmdBuf);
+	assert(res == VK_SUCCESS);
+
+	return cmdBuf;
+}
+
+VkSurfaceKHR GetSurface(HINSTANCE hinstance, HWND window, VkInstance &instance)
+{
+	//WIN32 Only
+	VkSurfaceKHR surface = 0;
+
+	VkWin32SurfaceCreateInfoKHR createInfo = {};
+	createInfo.sType = VK_STRUCTURE_TYPE_WIN32_SURFACE_CREATE_INFO_KHR;
+	createInfo.pNext = NULL;
+	createInfo.hinstance = hinstance;
+	createInfo.hwnd = window;
+	
+	VkResult res = vkCreateWin32SurfaceKHR(instance, &createInfo, NULL, &surface);
+	assert(res == VK_SUCCESS);
+
+	return surface;
+}
+
+uint32_t GetGraphicsPresentQueueIndex(
+	const uint32_t queueCount, VkPhysicalDevice gpu, 
+	vector<VkQueueFamilyProperties> queueProps, VkSurfaceKHR surface)
+{
+	VkBool32 *supportsPresent =
+		(VkBool32 *)malloc(queueCount * sizeof(VkBool32));
+	for (uint32_t i = 0; i < queueCount; i++) {
+		vkGetPhysicalDeviceSurfaceSupportKHR(gpu, i, surface,
+			&supportsPresent[i]);
+	}
+
+	// Search for a graphics queue and a present queue in the array of queue
+	// families, try to find one that supports both
+	uint32_t graphicsQueueNodeIndex = UINT32_MAX;
+	for (uint32_t i = 0; i < queueCount; i++) {
+		if ((queueProps[i].queueFlags & VK_QUEUE_GRAPHICS_BIT) != 0) {
+			if (supportsPresent[i] == VK_TRUE) {
+				graphicsQueueNodeIndex = i;
+				break;
+			}
+		}
+	}
+	free(supportsPresent);
+
+	// Generate error if could not find a queue that supports both a graphics
+	// and present
+	if (graphicsQueueNodeIndex == UINT32_MAX) {
+		std::cout << "Could not find a queue that supports both graphics and "
+			"present\n";
+		exit(-1);
+	}
+
+	return graphicsQueueNodeIndex;
+}
+
+VkSwapchainKHR CreateSwapChain(
+	VkPhysicalDevice gpu, VkSurfaceKHR surface, VkFormat &format,
+	int width, int height,
+	VkDevice device,
+	uint32_t &swapChainImageCount, vector<swap_chain_buffer> &buffer,
+	VkCommandBuffer &cmdBuf, VkQueue &queue,
+	const uint32_t queueFamilyIndex)
+{
+	// Get the list of VkFormats that are supported:
+	uint32_t formatCount;
+	VkResult res = 
+		vkGetPhysicalDeviceSurfaceFormatsKHR(gpu, surface, &formatCount, NULL);
+	assert(res == VK_SUCCESS);
+
+	VkSurfaceFormatKHR *surfFormats =
+		(VkSurfaceFormatKHR *)malloc(formatCount * sizeof(VkSurfaceFormatKHR));
+	res = vkGetPhysicalDeviceSurfaceFormatsKHR(gpu, surface,
+		&formatCount, surfFormats);
+	assert(res == VK_SUCCESS);
+
+	// If the format list includes just one entry of VK_FORMAT_UNDEFINED,
+	// the surface has no preferred format.  Otherwise, at least one
+	// supported format will be returned.
+	if (formatCount == 1 && surfFormats[0].format == VK_FORMAT_UNDEFINED) {
+		format = VK_FORMAT_B8G8R8A8_UNORM;
+	}
+	else {
+		assert(formatCount >= 1);
+		format = surfFormats[0].format;
+	}
+
+
+	VkSurfaceCapabilitiesKHR surfCapabilities;
+
+	res = vkGetPhysicalDeviceSurfaceCapabilitiesKHR(gpu, surface,
+		&surfCapabilities);
+	assert(res == VK_SUCCESS);
+
+	uint32_t presentModeCount;
+	res = vkGetPhysicalDeviceSurfacePresentModesKHR(gpu, surface,
+		&presentModeCount, NULL);
+	assert(res == VK_SUCCESS);
+
+	VkPresentModeKHR *presentModes =
+		(VkPresentModeKHR *)malloc(presentModeCount * sizeof(VkPresentModeKHR));
+
+	res = vkGetPhysicalDeviceSurfacePresentModesKHR(
+		gpu, surface, &presentModeCount, presentModes);
+	assert(res == VK_SUCCESS);
+
+	VkExtent2D swapChainExtent;
+	// width and height are either both -1, or both not -1.
+	if (surfCapabilities.currentExtent.width == (uint32_t)-1) {
+		// If the surface size is undefined, the size is set to
+		// the size of the images requested.
+		swapChainExtent.width = width;
+		swapChainExtent.height = height;
+	}
+	else {
+		// If the surface size is defined, the swap chain size must match
+		swapChainExtent = surfCapabilities.currentExtent;
+	}
+
+	// If mailbox mode is available, use it, as is the lowest-latency non-
+	// tearing mode.  If not, try IMMEDIATE which will usually be available,
+	// and is fastest (though it tears).  If not, fall back to FIFO which is
+	// always available.
+	VkPresentModeKHR swapchainPresentMode = VK_PRESENT_MODE_FIFO_KHR;
+	for (size_t i = 0; i < presentModeCount; i++) {
+		if (presentModes[i] == VK_PRESENT_MODE_MAILBOX_KHR) {
+			swapchainPresentMode = VK_PRESENT_MODE_MAILBOX_KHR;
+			break;
+		}
+		if ((swapchainPresentMode != VK_PRESENT_MODE_MAILBOX_KHR) &&
+			(presentModes[i] == VK_PRESENT_MODE_IMMEDIATE_KHR)) {
+			swapchainPresentMode = VK_PRESENT_MODE_IMMEDIATE_KHR;
+		}
+	}
+
+	// Determine the number of VkImage's to use in the swap chain (we desire to
+	// own only 1 image at a time, besides the images being displayed and
+	// queued for display):
+	uint32_t desiredNumberOfSwapChainImages =
+		surfCapabilities.minImageCount + 1;
+	if ((surfCapabilities.maxImageCount > 0) &&
+		(desiredNumberOfSwapChainImages > surfCapabilities.maxImageCount)) {
+		// Application must settle for fewer images than desired:
+		desiredNumberOfSwapChainImages = surfCapabilities.maxImageCount;
+	}
+
+	VkSurfaceTransformFlagBitsKHR preTransform;
+	if (surfCapabilities.supportedTransforms &
+		VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR) {
+		preTransform = VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR;
+	}
+	else {
+		preTransform = surfCapabilities.currentTransform;
+	}
+
+	VkSwapchainKHR swapChain;
+	VkSwapchainCreateInfoKHR swap_chain = {};
+	swap_chain.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+	swap_chain.pNext = NULL;
+	swap_chain.surface = surface;
+	swap_chain.minImageCount = desiredNumberOfSwapChainImages;
+	swap_chain.imageFormat = format;
+	swap_chain.imageExtent.width = swapChainExtent.width;
+	swap_chain.imageExtent.height = swapChainExtent.height;
+	swap_chain.preTransform = preTransform;
+	swap_chain.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+	swap_chain.imageArrayLayers = 1;
+	swap_chain.presentMode = swapchainPresentMode;
+	swap_chain.oldSwapchain = NULL;
+	swap_chain.clipped = true;
+	swap_chain.imageColorSpace = VK_COLORSPACE_SRGB_NONLINEAR_KHR;
+	swap_chain.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+	swap_chain.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+	swap_chain.queueFamilyIndexCount = 0;
+	swap_chain.pQueueFamilyIndices = NULL;
+
+	res =
+		vkCreateSwapchainKHR(device, &swap_chain, NULL, &swapChain);
+	assert(res == VK_SUCCESS);
+
+	res = vkGetSwapchainImagesKHR(device, swapChain,
+		&swapChainImageCount, NULL);
+	assert(res == VK_SUCCESS);
+
+	VkImage *swapchainImages =
+		(VkImage *)malloc(swapChainImageCount * sizeof(VkImage));
+	assert(swapchainImages);
+	res = vkGetSwapchainImagesKHR(device, swapChain,
+		&swapChainImageCount, swapchainImages);
+	assert(res == VK_SUCCESS);
+
+	buffer.resize(swapChainImageCount);
+
+	//Create Images
+	BeginCommandBuffer(cmdBuf);
+	vkGetDeviceQueue(device, queueFamilyIndex, 0,
+		&queue);
+
+	for (uint32_t i = 0; i < swapChainImageCount; i++) {
+		VkImageViewCreateInfo color_image_view = {};
+		color_image_view.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+		color_image_view.pNext = NULL;
+		color_image_view.format = format;
+		color_image_view.components.r = VK_COMPONENT_SWIZZLE_R;
+		color_image_view.components.g = VK_COMPONENT_SWIZZLE_G;
+		color_image_view.components.b = VK_COMPONENT_SWIZZLE_B;
+		color_image_view.components.a = VK_COMPONENT_SWIZZLE_A;
+		color_image_view.subresourceRange.aspectMask =
+			VK_IMAGE_ASPECT_COLOR_BIT;
+		color_image_view.subresourceRange.baseMipLevel = 0;
+		color_image_view.subresourceRange.levelCount = 1;
+		color_image_view.subresourceRange.baseArrayLayer = 0;
+		color_image_view.subresourceRange.layerCount = 1;
+		color_image_view.viewType = VK_IMAGE_VIEW_TYPE_2D;
+		color_image_view.flags = 0;
+
+		buffer[i].image = swapchainImages[i];
+
+		set_image_layout(cmdBuf, queue, buffer[i].image, VK_IMAGE_ASPECT_COLOR_BIT,
+			VK_IMAGE_LAYOUT_UNDEFINED,
+			VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL);
+
+		color_image_view.image = buffer[i].image;
+
+		res = vkCreateImageView(device, &color_image_view, NULL,
+			&buffer[i].view);
+		assert(res == VK_SUCCESS);
+	}
+
+	EndCommandBuffer(cmdBuf);
+	QueueCommandBuffer(cmdBuf, device, queue);
+
+	return swapChain;
+}
 
 /*
 	Main
@@ -215,12 +632,25 @@ VkDevice CreateDevice(vector<VkPhysicalDevice> &gpus, uint32_t &queueCount, vect
 
 int main(int argc, char *argv[])
 {
-	initWindow(800, 600, "ColoredCube");
+	InitWindow(width, height, "ColoredCube");
 
+	//Create Instance
 	m_instance = CreateInstance("Colored Cube");
 
+	//Enumerate & Create device
 	EnumeratePhysicalDevices(m_instance, m_GPUs);
-	m_device = CreateDevice(m_GPUs, m_queueCount, m_queueProps);
+	m_device = CreateDevice(m_GPUs, m_queueCount, m_queueProps, m_queueFamilyIndex);
+
+	//Init command buffer
+	m_cmdPool = CreateCommandPool(m_queueFamilyIndex, m_device);
+	m_cmdBuffer = CreateCommandBuffer(m_cmdPool, m_device);
+
+	//Init swapChain
+	m_surface = GetSurface(m_connection, m_window, m_instance);
+	m_queueGraphicsPresentIndex = GetGraphicsPresentQueueIndex(m_queueCount, m_GPUs[0], m_queueProps, m_surface);
+	m_swapChain = CreateSwapChain(
+		m_GPUs[0], m_surface, m_colorImgFormat, width, height, m_device,
+		m_swapChainImageCount, m_swapChainImgBuffer, m_cmdBuffer, m_queue, m_queueFamilyIndex);
 
 	//todo...
 }
